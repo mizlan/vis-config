74a1b7d9996626040a1ef41ab35b4e95889e4849
Michael Lan <michaellan202@gmail.com>
2020-12-07 11:24:06 -0800

    more changes

--- visrc.lua
+++ visrc.lua
@@ -1,19 +1,14 @@
 require('vis')
-
 require('vis-commentary')
 require('vis-surround')
 require('vis-ibrace')
 require('vis-status')
 
-require('vis-lint-manager')
-
--- this disables auto ci" jumping, so comment it out for now
--- local p = require('vis-pairs')
--- p.autopairs = false
+-- require('vis-pairs')
+-- require('vis-lint-manager')
 
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
-	vis:command('set theme moon')
+	vis:command('set theme zenburn')
 	vis:command('set escdelay 20')
 	vis:command('set tabwidth 2')
 	vis:command('set autoindent')
@@ -21,11 +16,8 @@ vis.events.subscribe(vis.events.INIT, function()
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
 	vis:command('set number')
 	vis:command('set rnu')
-	vis:command('set show-tabs on')
 	vis:command('set show-eof off')
+	vis:command('set cursorline')
 end)
-
