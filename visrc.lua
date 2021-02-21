da9a016ab547b7636209ddd2999032e171049dd5
Michael Lan <michaellan202@gmail.com>
2021-02-21 13:30:04 -0800

    more miscellaneous changes

--- visrc.lua
+++ visrc.lua
@@ -1,23 +1,31 @@
-require('vis')
-require('vis-commentary')
-require('vis-surround')
-require('vis-ibrace')
-require('vis-status')
+require("vis")
+require("vis-commentary")
+require("vis-surround")
+require("vis-ibrace")
+require("vis-status")
+require("vis-brittany")
 
 -- require('vis-pairs')
 -- require('vis-lint-manager')
 
-vis.events.subscribe(vis.events.INIT, function()
-	vis:command('set theme zenburn')
-	vis:command('set escdelay 20')
-	vis:command('set tabwidth 2')
-	vis:command('set autoindent')
-	vis:command('set expandtab')
-end)
+vis.events.subscribe(
+    vis.events.INIT,
+    function()
+        vis:command("set theme moon")
+        -- vis:command('set theme zenburn')
+        vis:command("set escdelay 20")
+        vis:command("set tabwidth 2")
+        vis:command("set autoindent")
+        vis:command("set expandtab")
+    end
+)
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set number')
-	vis:command('set rnu')
-	vis:command('set show-eof off')
-	vis:command('set cursorline')
-end)
+vis.events.subscribe(
+    vis.events.WIN_OPEN,
+    function(win)
+        -- vis:command('set number')
+        -- vis:command('set rnu')
+        vis:command("set show-eof off")
+        -- vis:command('set cursorline')
+    end
+)
